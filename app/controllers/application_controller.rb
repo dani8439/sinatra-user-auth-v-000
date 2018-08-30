fb37d3388ba06aa360c0da6c5647507c5f57e8c2
Dani Schuhman <dani8439@aol.com>
2018-08-30 19:24:55 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -19,7 +19,7 @@ class ApplicationController < Sinatra::Base
     @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     @user.save
     session[:id] = @user.id
-    
+
     redirect '/users/home'
   end
 
@@ -28,6 +28,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/sessions' do
+    @user = User.find_by(email: params["email"])
 
     redirect '/users/home'
   end
