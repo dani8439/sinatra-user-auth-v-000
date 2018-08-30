770ffa07a77833cc6037d826e249142db1321d67
Dani Schuhman <dani8439@aol.com>
2018-08-30 19:14:42 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,8 +16,7 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/registrations' do
-    puts params
-
+    @user = User.new(name: params["name"], email: params["email"], password: params["password"])
     redirect '/users/home'
   end
 
@@ -30,7 +29,7 @@ class ApplicationController < Sinatra::Base
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
 
     redirect '/'
   end
